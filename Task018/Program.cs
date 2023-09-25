e0e72aeb83a0eb73dbe8dd0247784f1ae685d0e2
AlexG <gusev_alexey@mail.ru>
2023-09-25 21:28:27 +0300

    after seminar

---
+++ Task018/Program.cs
@@ -0,0 +1,60 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Write("Введите номер четверти: ");
+int num = int.Parse(Console.ReadLine());
+
+if(num == 1)
+{
+    Console.WriteLine("x > 0, y > 0");
+}
+else if(num == 2)
+{
+    Console.WriteLine("x < 0, y > 0");
+}
+else if(num == 3)
+{
+    Console.WriteLine("x < 0, y < 0");
+}
+else if(num == 4)
+{
+    Console.WriteLine("x > 0, y < 0");
+}
+else
+{
+    Console.WriteLine("Введите значение от 1 до 4");
+}
+
+//---------------------------
+
+Console.Write("Введите номер четверти, по которой хотите получить координаты: ");
+int number = int.Parse(Console.ReadLine());
+
+switch (number)
+{
+    case 1:
+    {
+       Console.WriteLine("В первой четверти значения х > 0, y > 0");
+       break; 
+    }
+    case 2:
+    {
+        Console.WriteLine("В первой четверти значения х < 0, y > 0");
+        break;
+    }
+    case 3:
+    {
+        Console.WriteLine("В первой четверти значения х < 0, y < 0");
+        break;
+    }
+    case 4:
+    {
+       Console.WriteLine("В первой четверти значения х > 0, y < 0");
+       break;
+    }
+    
+    default:
+    {
+        Console.WriteLine("Значение не верное");
+        break; 
+    }
+}
---
+++ Task020/Program.cs
@@ -0,0 +1,17 @@
+﻿// // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+Console.Write("Введите координаты Х, точки А: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Введите координаты Y, точки А: ");
+int y1 = int.Parse(Console.ReadLine());
+Console.Write("Введите координаты Х, точки B: ");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("Введите координаты Y, точки B: ");
+int y2 = int.Parse(Console.ReadLine());
+
+double c = Math.Sqrt (Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
+
+System.Console.WriteLine($"Расстояние между координатами: {c}");
